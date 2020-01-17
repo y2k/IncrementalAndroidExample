587069d098d9ca8c92590f9240030022b1f4a7ef
y2k <itwisterlx@gmail.com>
2020-01-17 22:51:50 +0300

    refactoring

--- IncrementalAndroidExample/MainActivity.fs
+++ IncrementalAndroidExample/MainActivity.fs
@@ -8,10 +8,16 @@ type MainActivity() =
     inherit Activity()
     override this.OnCreate(bundle) =
         base.OnCreate(bundle)
-        let rec reloadUi() =
+
+        let mutable reloadUiF = ignore
+        let r =
             UpDownScreen.view' this (fun f _ ->
                 UpDownScreen.update f
-                reloadUi())
+                reloadUiF())
+
+        let rec reloadUi() =
+            r
             |> AVal.force
             |> this.SetContentView
+        reloadUiF <- reloadUi
         reloadUi()
